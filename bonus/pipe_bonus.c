4a411ae71622fb84cdce55985eb17b8ebc4a64be
Houssame El Bandoudi <hel-band@e3r4p1.1337.ma>
2024-06-24 12:06:53 +0100

    update

--- bonus/pipe_bonus.c
+++ bonus/pipe_bonus.c
@@ -1,5 +1,36 @@
 #include "pipex_bonus.h"
 
+void here_doc(char *limiter) {
+    char buffer[1024];
+    int temp_file = open(".temp_heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    ssize_t read_bytes;
+    size_t limiter_len = ft_strlen(limiter);
+
+    if (temp_file < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        write(STDOUT_FILENO, "> ", 2);
+        read_bytes = read(STDIN_FILENO, buffer, 1024);
+        if (read_bytes < 0) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        if (read_bytes == 0) {
+            break;
+        }
+        buffer[read_bytes] = '\0';
+        if (ft_strncmp(buffer, limiter, limiter_len) == 0 && buffer[limiter_len] == '\n') {
+            break;
+        }
+        write(temp_file, buffer, read_bytes);
+    }
+
+    close(temp_file);
+}
+
 void execute_command(char *cmd, char **envp) {
     parsing(cmd, envp);
 }
@@ -14,18 +45,36 @@ void child_process(int infile, int outfile, char *cmd, char **envp) {
     execute_command(cmd, envp);
 }
 
+void f ()
+{
+    system("lsof -c pipex");
+}
+
 void pipex(int argc, char **argv, char **envp) {
+    atexit(f);
     int pipes[2];
-    int infile = open(argv[1], O_RDONLY);
-    int outfile = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    int infile, outfile;
+    int i;
+    int fd_in;
     pid_t pid;
-    int i = 2;
-    int fd_in = infile;
+
+    if (ft_strncmp(argv[1], "here_doc", 8) == 0) {
+        here_doc(argv[2]);
+        infile = open(".temp_heredoc", O_RDONLY);
+        outfile = open(argv[argc - 1], O_CREAT | O_WRONLY | O_APPEND, 0644);
+        i = 3;
+    } else {
+        infile = open(argv[1], O_RDONLY);
+        outfile = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        i = 2;
+    }
 
     if (infile < 0 || outfile < 0) {
         perror("open");
         exit(EXIT_FAILURE);
     }
+
+    fd_in = infile;
 
     while (i < argc - 2) {
         if (pipe(pipes) == -1) {
@@ -63,14 +112,14 @@ void pipex(int argc, char **argv, char **envp) {
     close(fd_in);
     close(outfile);
 
-    while (wait(NULL) > 0);
+    while (waitpid(-1, NULL, 0) > 0);
 }
 
 int main(int argc, char **argv, char **envp) {
-    if (argc < 5) {
-        fprintf(stderr, "Usage: %s file1 cmd1 cmd2 ... cmdn file2\n", argv[0]);
-        return 1;
+    if (argc < 5 || (ft_strncmp(argv[1], "here_doc", 8) == 0 && argc < 6)) {
+        fprintf(stderr, "Usage: %s [here_doc LIMITER] file1 cmd1 cmd2 ... cmdn file2\n", argv[0]);
+        return EXIT_FAILURE;
     }
     pipex(argc, argv, envp);
-    return 0;
+    return EXIT_SUCCESS;
 }
